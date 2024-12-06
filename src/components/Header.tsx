3e2c177a47a85a637aeb434575422e932f2f0db9
Murat <7101489+murat-mehmet@users.noreply.github.com>
2024-12-06 09:43:08 +0000

    fix: set icon object-fit to cover

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -33,6 +33,7 @@ export default function Header({ shareData }) {
             style={{
               width: "100%",
               height: "100%",
+              objectFit: 'cover'
             }}
           />
         </div>
